7084040d62edad1f0fcaa88c2ec4864a8016629b
Bob <bob@reefscape.net>
2022-09-20 22:49:30 +0200

    add simple template

---
+++ sketch-template.js
@@ -0,0 +1,196 @@
+p5.disableFriendlyErrors = false; // disables FES to speed things up a little bit
+
+let toko = new Toko();
+let particles = [];
+let attractors = [];
+
+function preload() {
+  //
+  // All loading calls here
+  //
+}
+
+function setup() {
+
+  //------------------------------------------------------
+  //
+  //  set base canvas
+  //
+  let sketchElementId = "sketch-canvas";
+  let canvasWidth = 0;
+  let canvasHeight = 0;
+
+  //
+  //  the size is set using the Toko setup options
+  //
+  p5Canvas = createCanvas(canvasWidth, canvasHeight, P2D);
+  p5Canvas.parent(sketchElementId);
+
+  //-------------------------------------------------------
+  //
+  //  start Toko
+  //
+  toko.setup({
+    //
+    //  basic options
+    //
+    title: "Toko template",                 //  title displayed
+    sketchElementId: sketchElementId,   //  id used to create the p5 canvas
+    canvasSize: toko.SIZE_DEFAULT,      //  canvas size to use
+    //
+    //  additional options
+    //
+    showSaveSketchButton: true,         //  show save image button in tweakpane
+    saveSettingsWithSketch: true,       //  save json of settings together with the image
+    acceptDroppedSettings: true,        //  accept dropped json files with settings
+    useParameterPanel: true,            //  use the tweakpane panel for settings
+    showAdvancedOptions: true,          //  show advanced settings in tweakpane, like size
+    captureFrames: false,               //  no record option
+  });
+
+  //
+  //-------------------------------------------------------
+  //
+  //  sketch parameters
+  //
+  p = {
+    steps: 10,
+    colors: 'westCoast',
+    interpolated: false,
+  }
+
+  let palettes = toko.getPaletteSelection('basic, golid, metbrewer', false, true);
+
+  //
+  //  set the tweakpane controls
+  //
+  toko.pane.tab.addInput(p, 'colors', {
+    options:palettes
+  })
+  toko.pane.tab.addInput(p, 'steps', { min: 2, max: 40, step: 1});
+  toko.pane.tab.addInput(p, 'interpolated');
+
+  //
+  //  listen to tweakpane changes
+  //
+  toko.pane.events.on("change", (value) => {
+    refresh();
+  });
+  
+  refresh();
+  noLoop();
+
+  //---------------------------------------------
+  toko.endSetup();
+  //---------------------------------------------
+}
+
+function refresh() {
+  //
+  //  set domain range to number of steps
+  //
+  const o = {
+    domain: [0, p.steps*p.steps],
+  }
+  //
+  //  get colors
+  //
+  colors = toko.getColorScale(this.p.colors,o);
+  //
+  //  redraw with updated parameters
+  //
+  redraw();
+}
+
+function draw() {
+  //---------------------------------------------
+  toko.startDraw(); // do not remove
+  //---------------------------------------------
+  
+  clear();
+  stroke('#fff');
+  strokeWeight(2);
+
+  let w = width/p.steps;
+  let h = height/p.steps;
+
+  //
+  //  draw a grid with the colors from the palette
+  //
+  for (let i = 0; i < p.steps; i++) {
+    for (let j = 0; j < p.steps; j++) {
+      if (p.interpolated) {
+        fill(colors.scale(i+j*p.steps));
+      } else {
+        fill(colors.originalScale(i+j*p.steps));
+      }
+      
+      rect(i*w,j*h,w,h);
+    }
+  }
+
+  //---------------------------------------------
+  toko.endDraw(); // do not remove
+  //---------------------------------------------
+}
+
+function attractorForce(attractor, position) {
+  let force = p5.Vector.sub(attractor, position);
+  let distanceSq = constrain(force.magSq(), 100, 1000);
+  let strength = p.gravity / distanceSq;
+  force.setMag(strength);
+  return force;
+}
+
+function saveSettings() {
+  toko.saveSettings();
+}
+
+//---------------------------------------------
+//
+//  EVENTS
+//
+//---------------------------------------------
+
+function captureStarted() {
+  //
+  //  called when capture has started, use to reset visuals
+  //
+  console.log("Toko - captureStarted");
+}
+
+function captureStopped() {
+  //
+  //  called when capture is stopped, use to reset visuals
+  //
+  console.log("Toko - captureStopped");
+}
+
+function canvasResized() {
+  //
+  //  called when the canvas was resized
+  //
+  console.log("Toko - canvasResized");
+}
+
+function windowResized() {
+  //
+  //  resize the canvas when the framing div was resized
+  //
+  console.log("Toko - windowResized");
+
+  var newWidth = document.getElementById("sketch-canvas").offsetWidth;
+  var newHeight = document.getElementById("sketch-canvas").offsetHeight;
+
+  if (newWidth != width || newHeight != height) {
+    canvasResized();
+  }
+}
+
+function receivedFile(file) {
+  //
+  //  called when a JSON file is dropped on the sketch
+  //  tweakpane settings are automatically updated
+  //
+  console.log("Toko - receivedFile")
+}
