0c365e1bbef0b7d5f37ef6f922013ee89d111382
Bob <bob@reefscape.net>
2023-05-03 21:40:14 +0200

    add seeded random function

--- assets/toko/dist/Toko.js
+++ assets/toko/dist/Toko.js
@@ -3209,6 +3209,20 @@ var Toko = (function () {
   };
 
   //
+  //  return seeded random function
+  //  see https://github.com/cprosche/mulberry32
+  //
+  Toko.rand = function (seed) {
+    if (seed == undefined) { seed = Date.now(); }
+    return function() {
+      let t = seed += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+  };
+
+  //
   //  additions to TweakPane
   //
 
--- assets/toko/source/core/math.js
+++ assets/toko/source/core/math.js
@@ -17,3 +17,18 @@ Toko.prototype.wrap = function (value, min = 0, max = 100) {
 
   return vw
 }
+
+//
+//  return seeded random function
+//  see https://github.com/cprosche/mulberry32
+//
+Toko.rand = function (seed) {
+  if (seed == undefined) { seed = Date.now(); }
+  return function() {
+    let t = seed += 0x6D2B79F5;
+    t = Math.imul(t ^ t >>> 15, t | 1);
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+  }
+}
+
