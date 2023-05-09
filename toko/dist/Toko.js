1b927ada1bcae913c13d1333035fcd5b9de1657c
Bob <bob@reefscape.net>
2023-05-09 22:57:55 +0200

    clarify random function

--- toko/dist/Toko.js
+++ toko/dist/Toko.js
@@ -3292,6 +3292,7 @@ var Toko = (function () {
 
   //
   //  pick a random adjective from the list
+  //  note this does not use the seeded random function to avoid file name conflicts
   //
   Toko.prototype.randomAdjective = function() {
     return this.ADJECTIVES[Math.floor(this.ADJECTIVES.length * Math.random())];
@@ -3299,6 +3300,7 @@ var Toko = (function () {
 
   //
   //  pick a random noun from the list
+  //  note this does not use the seeded random function to avoid file name conflicts
   //
   Toko.prototype.randomNoun = function() {
     return this.NOUNS[Math.floor(this.NOUNS.length * Math.random())];
