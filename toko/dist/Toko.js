7d90a24f4659f086984fe76e4f8dcd9b405bb517
Bob <bob@reefscape.net>
2023-12-22 18:16:43 +0100

    add lospec palettes

--- toko/dist/Toko.js
+++ toko/dist/Toko.js
@@ -3398,6 +3398,140 @@ var Toko = (function () {
     },
   ];
 
+  //
+  // Selected palettes from lospec
+  // https://lospec.com/palette-list
+  //
+  var lospecPalettes = [
+    {
+      name: 'sweetie16', // https://lospec.com/palette-list/sweetie-16
+      colors: [
+        '#5d275d',
+        '#b13e53',
+        '#ef7d57',
+        '#ffcd75',
+        '#a7f070',
+        '#38b764',
+        '#257179',
+        '#29366f',
+        '#3b5dc9',
+        '#41a6f6',
+        '#73eff7',
+        '#94b0c2',
+        '#566c86',
+        '#333c57',
+      ],
+
+      stroke: '#f4f4f4',
+      background: '#1a1c2c',
+      type: 'lospec',
+    },
+    {
+      name: 'na16', // https://lospec.com/palette-list/na16
+      colors: [
+        '#8c8fae',
+        '#584563',
+        '#3e2137',
+        '#9a6348',
+        '#d79b7d',
+        '#f5edba',
+        '#c0c741',
+        '#647d34',
+        '#e4943a',
+        '#9d303b',
+        '#d26471',
+        '#70377f',
+        '#7ec4c1',
+        '#34859d',
+        '#17434b',
+        '#1f0e1c',
+      ],
+      stroke: '#f5edba',
+      background: '#1f0e1c',
+      type: 'lospec',
+    },
+    {
+      name: 'lost-century', // https://lospec.com/palette-list/lost-century
+      colors: [
+        '#d1b187',
+        '#c77b58',
+        '#ae5d40',
+        '#79444a',
+        '#4b3d44',
+        '#ba9158',
+        '#927441',
+        '#4d4539',
+        '#77743b',
+        '#b3a555',
+        '#d2c9a5',
+        '#8caba1',
+        '#4b726e',
+        '#574852',
+        '#847875',
+        '#ab9b8e',
+      ],
+      type: 'lospec',
+    },
+    {
+      name: 'nostalgic-dreams', // https://lospec.com/palette-list/nostalgic-dreams
+      colors: [
+        '#d9af80',
+        '#b07972',
+        '#524352',
+        '#686887',
+        '#7f9bb0',
+        '#bfd4b0',
+        '#90b870',
+        '#628c70',
+      ],
+      type: 'lospec',
+    },
+    {
+      name: 'sls08', // https://lospec.com/palette-list/slso8
+      colors: [
+        '#0d2b45',
+        '#203c56',
+        '#544e68',
+        '#8d697a',
+        '#d08159',
+        '#ffaa5e',
+        '#ffd4a3',
+        '#ffecd6',
+      ],
+      type: 'lospec',
+    },
+    {
+      name: 'rust-gold-8', // https://lospec.com/palette-list/rust-gold-8
+      colors: [
+        '#f6cd26',
+        '#ac6b26',
+        '#563226',
+        '#331c17',
+        '#bb7f57',
+        '#725956',
+        '#393939',
+        '#202020',
+      ],
+      type: 'lospec',
+    },
+    {
+      name: 'ink-crimson', // https://lospec.com/palette-list/ink-crimson
+      colors: [
+        '#ff0546',
+        '#9c173b',
+        '#660f31',
+        '#450327',
+        '#270022',
+        '#17001d',
+        '#09010d',
+        '#0ce6f2',
+        '#0098db',
+        '#1e579c',
+      ],
+      type: 'lospec',
+    },
+  ];
+
   Toko.prototype.CONTRAST_MIX_COLORS = ['#111', '#eee'];
   Toko.prototype.CONTRAST_MIX_FACTOR = 0.8;
   Toko.prototype.CONTRAST_MIX_MODE = 'lab';
@@ -3767,6 +3901,7 @@ var Toko = (function () {
       judsonPalettes,
       jungPalettes,
       kovecsesPalettes,
+      lospecPalettes,
       mayoPalettes,
       metBrewerPalettes,
       orbifoldPalettes,
