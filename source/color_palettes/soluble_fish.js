b7f7de7be0931f879e926cb5f26f2fed09de0494
Bob <bob@reefscape.net>
2024-08-31 10:58:20 +0200

    add soluble fish palettes

---
+++ source/color_palettes/soluble_fish.js
@@ -0,0 +1,269 @@
+//
+// Original collection by Aaron Reuland / A Soluble Fish
+// https://openprocessing.org/sketch/2176841/
+// License: CreativeCommons Attribution NonCommercial ShareAlike
+//
+export default [
+  { colors: ['#ccaa98', '#afa19c', '#ff5558', '#b42e50', '#463c39'], name: 'cranberry_stone', type: 'soluble_fish' },
+  {
+    colors: ['#c3ad8c', '#c89b81', '#d4c167', '#766a6a', '#384250'],
+    name: 'muted_wood_and_stone',
+    type: 'soluble_fish',
+  },
+  { colors: ['#e8e1d4', '#b3bfbb', '#996e40', '#815731', '#3b332d'], name: 'wood and stone', type: 'soluble_fish' },
+  {
+    colors: ['#E3DED5', '#abaaa8', '#cdb67b', '#a07b42', '#3d3b3a'],
+    name: 'muted gold and black',
+    type: 'soluble_fish',
+  },
+  { colors: ['#fa5e04', '#db3004', '#ac4d1d', '#634740', '#5b1602'], name: 'flaming fall', type: 'soluble_fish' },
+  { colors: ['#cac9cc', '#758f96', '#393c25', '#bc3e10', '#5f2223'], name: 'frosted fall leaf', type: 'soluble_fish' },
+  { colors: ['#1587b3', '#51cee7', '#f889b9', '#ee390c', '#2c0505'], name: 'warm and felty', type: 'soluble_fish' },
+  { colors: ['#a8adaf', '#718a6a', '#072376', '#9e6b03', '#8c1108'], name: 'somehow european', type: 'soluble_fish' },
+  { colors: ['#fcb86a', '#904a00', '#4c2500', '#b3b082', '#6f4e12'], name: 'rusted tank', type: 'soluble_fish' },
+  { colors: ['#f1ec78', '#85b19f', '#6d5456', '#3d4f49', '#3b3c41'], name: 'lichen wood', type: 'soluble_fish' },
+  {
+    colors: ['#e0563a', '#a35f00', '#e2a355', '#aaba60', '#5d4740'],
+    name: 'very gentle, almost fragile, woody',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#775834', '#a67448', '#c19964', '#edd4aa', '#857c33'],
+    name: 'woodland floor, gentle',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#271f17', '#014e68', '#8497a8', '#c5cecf', '#b8ad38', '#978c0e'],
+    name: 'opulent silk',
+    type: 'soluble_fish',
+  },
+  { colors: ['#ffd283', '#fdbb81', '#d7ada7', '#afd7ec', '#7496b8'], name: 'golden hour clouds', type: 'soluble_fish' },
+  {
+    colors: ['#e8b145', '#af7f3a', '#924740', '#706b86', '#48576a', '#373f42'],
+    name: 'last leaf on the tree',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#61b2fa', '#8ddcff', '#545d5f', '#4a403c', '#b48551', '#1a4670', '#00264e'],
+    name: 'crisp november day, pond',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#f6dba3', '#aeb4b8', '#4c747f', '#474d4b', '#8e5c82', '#464664', '#30363d'],
+    name: 'cool purple green leaf',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#153660', '#1b1b12', '#899992', '#9d917b', '#aa7d5a', '#3a443a'],
+    name: 'vintage Japanese print',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#114d72', '#3c7b85', '#4c8981', '#a39f65', '#9acc74', '#041732'],
+    name: 'sisters makeup',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#131814', '#1f2f4f', '#557867', '#ad5b30', '#885330', '#c09978', '#bdaf94'],
+    name: 'Brighter Japanese print',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#baa21f', '#776b21', '#494636', '#807967', '#427289'],
+    name: 'acidy greens w browns',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#94472f', '#0e5264', '#522829', '#20283a', '#c97a5a', '#debe98'],
+    name: 'church window drawing',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#f7d299', '#d9d4c7', '#bcc3d0', '#f5ab84', '#c39089', '#32414e', '#34272a'],
+    name: 'sunset smoke',
+    type: 'soluble_fish',
+  },
+  { colors: ['#b15839', '#516e5f', '#395c74', '#b3864c', '#b0a37f'], name: 'American Harvest', type: 'soluble_fish' },
+  {
+    colors: ['#6e0f16', '#c12c0e', '#b47b00', '#e6a133', '#6f83a6', '#434047'],
+    name: 'angelic host',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#322b27', '#367185', '#828e8d', '#a29a6b', '#bf4622', '#ad6332'],
+    name: 'Breugels',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#2b4996', '#4c3813', '#e1862a', '#e25a45', '#859052', '#9c5b1f'],
+    name: 'German Expressionism',
+    type: 'soluble_fish',
+  },
+  { colors: ['#4e5a5a', '#919898', '#fff0b6', '#964301', '#560000'], name: 'agate layers', type: 'soluble_fish' },
+  {
+    colors: ['#c8a69a', '#781a00', '#9d4500', '#c97018', '#9b6626', '#ededf1', '#423e39', '#b68200'],
+    name: 'more agate',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#2d2a19', '#596760', '#a2b6ab', '#668487', '#d9e6db', '#f8fce9'],
+    name: 'grey sky /faint sun',
+    type: 'soluble_fish',
+  },
+  { colors: ['#512e18', '#c38f86', '#ebcbb7', '#99815b', '#7a6764'], name: 'winters leaves', type: 'soluble_fish' },
+  { colors: ['#dd841f', '#0f202e', '#862534', '#116887', '#304fad'], name: 'advance base', type: 'soluble_fish' },
+  { colors: ['#f5693e', '#085a8f', '#ab5567', '#81afd1', '#84565f'], name: 'brilliant', type: 'soluble_fish' },
+  { colors: ['#af4c24', '#8e6438', '#c58b31', '#768860', '#396872', '#1b344e'], name: 'thready', type: 'soluble_fish' },
+  {
+    colors: ['#82964e', '#567963', '#a7f7f7', '#5981a5', '#264b5d', '#323227'],
+    name: 'patina & moss',
+    type: 'soluble_fish',
+  },
+  { colors: ['#0d101c', '#554f4b', '#73757c', '#c8c3bb', '#faf2d4'], name: 'winters hope', type: 'soluble_fish' },
+  {
+    colors: ['#b9afac', '#9a7946', '#7e7252', '#757864', '#85260d', '#6f3929'],
+    name: 'burgundy bird',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#e8bc57', '#af9866', '#696563', '#945b0f', '#615e01', '#171d1d'],
+    name: 'cowboy sky',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#f6ac2c', '#b7656b', '#05729b', '#5e899e', '#601d24', '#8ea400', '#03417b'],
+    name: 'simple things',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#44291b', '#534841', '#724838', '#c74731', '#d66f4d', '#d9baa2', '#e1dcb6'],
+    name: 'old world plaster',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#fffafe', '#dfe2e8', '#838590', '#252b3d', '#f9ba23', '#b63c17', '#e00902'],
+    name: 'frost and fire',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#c47a70', '#625a5b', '#5d3334', '#89413b', '#b4af9c', '#918a98', '#d3d2d0'],
+    name: 'wet wood',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#416072', '#4c798c', '#ca9271', '#da440a', '#b90d04', '#980e03'],
+    name: 'bright infection',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#57331b', '#6f715a', '#9d8872', '#dec7b1', '#738884', '#384845'],
+    name: 'velvet forest',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#623028', '#aa5b54', '#7b541e', '#d9c49e', '#383c3b', '#809b95'],
+    name: 'worn soft',
+    type: 'soluble_fish',
+  },
+  { colors: ['#671318', '#960942', '#bf255f', '#bb7649', '#d49765'], name: 'gold and crimson', type: 'soluble_fish' },
+  {
+    colors: ['#3d4635', '#5a734c', '#8e472c', '#a7713b', '#252b21', '#304543', '#b18961', '#cab68c'],
+    name: 'faded forest',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#3b372a', '#a83e35', '#bb1e99', '#fdd312', '#4b3dd4', '#5bb0eb'],
+    name: 'peacock ore',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#c69322', '#c0bd9b', '#a08b8d', '#005e84', '#42a5e4', '#4ad3c8', '#02c287'],
+    name: 'opal',
+    type: 'soluble_fish',
+  },
+  { colors: ['#0c0c49', '#243487', '#6f90c9', '#75a8f5', '#CEC86B', '#815138'], name: 'lapis', type: 'soluble_fish' },
+  {
+    colors: ['#451c06', '#743212', '#9c400a', '#f97e01', '#929e78', '#13b2bc', '#0c8194'],
+    name: 'Norman Lewis',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#080d0a', '#515e57', '#f7de78', '#eecb43', '#ee970e', '#a98715', '#d4580c'],
+    name: 'La Puerto Del Sol',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#cb417f', '#e05ca4', '#edc0dd', '#deae94', '#cbf7c2', '#2b6860', '#1c0c0e'],
+    name: 'little love',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#82127e', '#1e396c', '#d51d67', '#ed8e07', '#d73a2a', '#291a2c'],
+    name: 'warm irridescence',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#385380', '#156ec9', '#3c92bd', '#347f92', '#7eaa96', '#857a76', '#942c26', '#cb7628'],
+    name: 'flicker on water',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#e70617', '#c15a01', '#458075', '#57d0c9', '#b7667d', '#ee80b3', '#b92b09', '#030003'],
+    name: 'bright little monster',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#c78b25', '#4e5b6d', '#636b4b', '#2b5c72', '#973535', '#ad9690', '#15171f'],
+    name: 'dreams of trees',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#747544', '#b0892e', '#fcae00', '#853628', '#ee3d0f', '#c05426', '#a34eae'],
+    name: 'french quarter at night',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#ea9751', '#fd8637', '#d3661c', '#b8593f', '#e61111', '#4a727a', '#086253', '#2e201f'],
+    name: 'french quarter in day',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#c7d9d2', '#889a65', '#437b9c', '#786c9a', '#c53c89', '#312457'],
+    name: 'Annas Hummingbird',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#013f61', '#07606a', '#0f7b9c', '#366a1c', '#eb7300', '#142027', '#e8eddd'],
+    name: 'Blue and Gold Macaw',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#2d4c61', '#7a6074', '#e7faa5', '#ede070', '#6ba6a8', '#376c94'],
+    name: 'Green Heron',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#600001', '#a8322f', '#d06b20', '#e1972f', '#ecede0', '#013c90', '#3d2842', '#a40211'],
+    name: 'Mandarin Duck',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#0a0d02', '#922301', '#f76e09', '#ead8b8', '#c0df80', '#a59b3c', '#86601a'],
+    name: 'Rufous-Crested Coquette',
+    type: 'soluble_fish',
+  },
+  { colors: ['#0b1438', '#1d2976', '#354994', '#b954f9', '#c3cfc4'], name: 'Satin Bower Bird', type: 'soluble_fish' },
+  {
+    colors: ['#47020e', '#884114', '#686963', '#66aec9', '#8ab2c0', '#e9f0e8', '#2f3b3e'],
+    name: 'White-bibbed Babbler',
+    type: 'soluble_fish',
+  },
+  {
+    colors: ['#8095b6', '#f7e3ac', '#546d85', '#c4b7aa', '#7791b4'],
+    name: 'sunset through clouds',
+    type: 'soluble_fish',
+  },
+  { colors: ['#a19938', '#47515d', '#c4b674', '#ac8567', '#5d6f97'], name: 'redwoods and ferns', type: 'soluble_fish' },
+  { colors: ['#693012', '#8ea080', '#5f4e20', '#74878c', '#b08c34'], name: 'beach rocks', type: 'soluble_fish' },
+  { colors: ['#8c0a53', '#d36c84', '#dbdb4a', '#ee0047', '#7c8a6b'], name: 'fuscia', type: 'soluble_fish' },
+];
